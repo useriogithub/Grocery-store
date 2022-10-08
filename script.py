5a9667dbc5ff97c22434d0b20a4b6c3a1707aa99
useriogithub <103942129+useriogithub@users.noreply.github.com>
2022-10-08 14:15:11 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+class Jerrys:
+  def __init__(self, grocery, produce, bakery, meat, deli, frozen, front_end):
+    self.grocery = grocery
+    self.produce = produce
+    self.bakery = bakery
+    self.meat = meat 
+    self.deli = deli
+    self.frozen = frozen
+    self.front_end = front_end
+
+class Shopper:
+  is_hungry = True
